5a6ae2e2f31fa2dd4d7033f9a08196c41e719993
liuyilun2000 <liuyilun2000@126.com>
2024-08-18 02:53:12 +0200

    lora train

--- llava_lora_128.sh
+++ llava_lora_128.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=a100          # Specify the partition or queue
 #SBATCH --gres=gpu:a100:8         # Request specific GPU resources
-#SBATCH --time=5:50:00           # Set a limit on the total run time
+#SBATCH --time=5:59:00           # Set a limit on the total run time
 #SBATCH --cpus-per-task=8         # Number of CPU cores per task
 #SBATCH --job-name=peft-lora-32-4
 #SBATCH --mail-type=ALL
@@ -11,9 +11,11 @@ source .bashrc
 python3 llava/llava_PEFT_init.py
 
 
+
 deepspeed llava/train_parallel_deepspeed_mixtral_lora.py --num_stages=8 \
-   --lora_r=32 --lora_alpha=64 \
-   --save_model_shard=100  --skip_shard=830 \
-   --checkpoint_dir=/home/atuin/b207dd/b207dd11/LLaVA-PEFT_lora_32_64_checkpoint 2>&1 | tee llava/train_parallel_deepspeed_mixtral_lora_32_64.log
+   --lora_r=128 --lora_alpha=256 \
+   --save_model_shard=100  --skip_shard=860  \
+   --checkpoint_dir=/home/atuin/b207dd/b207dd11/LLaVA-PEFT_lora_128_256_checkpoint 2>&1 | tee llava/train_parallel_deepspeed_mixtral_lora_128_256.log
 
 echo -e "\a"
+
